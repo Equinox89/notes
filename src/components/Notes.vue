01e006a88d04831bebc5dd71e43e20d61c055421
Oleh <oleh.datsiuk@p2h.com>
2022-12-25 14:09:00 +0200

    Note component

---
+++ src/components/Notes.vue
@@ -0,0 +1,60 @@
+<template>
+  <!--note list-->
+  <div class="notes">
+    <div class="note" v-for="(note, index) in notes" :key="index">
+      <div class="note-header">
+        <p>{{ note.title }}</p>
+      </div>
+      <div class="note-body">
+        <p>{{ note.description }}</p>
+        <span>{{ note.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: {
+      type: Array,
+      required: true
+    }
+  },
+  name: "Notes"
+}
+</script>
+
+<style lang="scss">
+  .notes {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 40px 0;
+  }
+
+  .note {
+    width: calc(50% - 8px);
+    padding: 18px 20px;
+    margin-bottom: 16px;
+    background-color: #ffffff;
+    border-radius: 8px;
+  }
+
+  .note-header {
+    p {
+      font-size: 24px;
+      color: #0949ec;
+    }
+  }
+  .note-body {
+    p {
+      margin: 16px 0;
+    }
+    span {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+</style>
